7caae27aa60a45e7a961d32326c1efaf27e30921
crsmith75 <45239533+crsmith75@users.noreply.github.com>
2019-09-25 17:38:33 -0400

    added chart functionality to gather time relevent data and covnerter for unix timestamp

--- index.js
+++ index.js
@@ -8,13 +8,31 @@ async function displayPrice(coin, date) {
         });
         // price = data.market_data.current_price
         console.log(data.data.market_data.current_price.usd)
-      }
+        }
     catch (err) {
         console.log('Error, err.message')
     }
+};
+displayPrice();
+
+async function displayChart(coin, date) {
+    try {
+        const chart = await CoinGeckoClient.coins.fetchMarketChart('bitcoin', {days:'0'});
+        price = chart.data.prices
+        price.forEach(price => console.log(price[0]))
+        }
+    //date is in unix timestamp
+    catch (err) {
+        console.log('Error, err.message')
     }
-  displayPrice();
+};
+displayChart();
+async function convertTimestamptoTime() {
+    unixTimestamp = 1569359448729;
+    dateObj = new Date(unixTimestamp * 1000); 
+    utcString = dateObj.toUTCString();
 
-  
-
- 
+    time = utcString;
+    console.log(time)
+  }
+  convertTimestamptoTime()
